5d16f867ae69990825af0170dfa8cc6f188daf46
golovech <love_2s5@naver.com>
2024-07-15 15:32:06 +0900

    Sample 코딩 추가
    Sample 코딩 추가

--- javaPro/src/javaPro/Sample.java
+++ javaPro/src/javaPro/Sample.java
@@ -5,7 +5,8 @@ public class Sample {
 	public static void main(String[] args) {
 		
 		System.out.println("최사랑");		
-		System.out.println("강아지");		
+		System.out.println("강아지");	
+		System.out.println("누군가에 의해 코딩 수정되어짐.");	
 
 	}
 
